da1c31de697a9c1a87b1073f1d19f708db9c1bc0
FernandodoPrado <n01329973@humbermail.ca>
2022-09-03 18:59:33 -0400

    Images added for each roll

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.diceroller
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 /*
 Makes it possible to roll a dice and see the output
@@ -13,7 +13,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val rollButton: Button = findViewById(R.id.button)
         rollButton.setOnClickListener { rollDice() }
-
+        rollDice()
     }
 /*
 Rolls the dice and updates the textview
@@ -25,12 +25,20 @@ Rolls the dice and updates the textview
     //creates a second dice from dice to have 20 sides
         val dice1 = Dice(20)
         val diceR = dice1.roll()
-    //sets the textview to the result of the random number
-        val resultTV: TextView = findViewById(R.id.textView)
-        resultTV.text = diceRoll.toString()
-        val resultTV1: TextView = findViewById(R.id.textView1)
-        resultTV1.text = diceR.toString()
+    //Update de dice image to the roll
+    val diceImage: ImageView = findViewById(R.id.imageView)
+    val drawableR = when (diceRoll) {
+        1 -> R.drawable.dice_1
+        2 -> R.drawable.dice_2
+        3 -> R.drawable.dice_3
+        4 -> R.drawable.dice_4
+        5 -> R.drawable.dice_5
+        else -> R.drawable.dice_6
     }
+    diceImage.setImageResource(drawableR)
+    diceImage.contentDescription = diceRoll.toString()
+
+}
 }
 
 class Dice(val numSides: Int) {
