9b3ea1082ff284ca2fce884c77bc2b0d3de7be2e
FernandodoPrado <n01329973@humbermail.ca>
2022-09-03 16:01:58 -0400

    Finished button and random number implementation

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.example.diceroller
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+/*
+Makes it possible to roll a dice and see the output
+ */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val rollButton: Button = findViewById(R.id.button)
+        rollButton.setOnClickListener { rollDice() }
+
+    }
+/*
+Rolls the dice and updates the textview
+ */
+    private fun rollDice() {
+    //creates an object from Dice to have 6 sides
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+    //sets the textview to the result of the random number
+        val resultTV: TextView = findViewById(R.id.textView)
+        resultTV.text = diceRoll.toString()
+    }
+}
+
+class Dice(val numSides: Int) {
+    fun roll(): Int {
+        return (1..numSides).random()
     }
 }
