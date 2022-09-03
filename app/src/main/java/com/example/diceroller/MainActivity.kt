73da95445814264421f8e02d30b1eb347f4b0fa2
FernandodoPrado <n01329973@humbermail.ca>
2022-09-03 16:28:46 -0400

    New Textview with random number up until 20

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -22,9 +22,14 @@ Rolls the dice and updates the textview
     //creates an object from Dice to have 6 sides
         val dice = Dice(6)
         val diceRoll = dice.roll()
+    //creates a second dice from dice to have 20 sides
+        val dice1 = Dice(20)
+        val diceR = dice1.roll()
     //sets the textview to the result of the random number
         val resultTV: TextView = findViewById(R.id.textView)
         resultTV.text = diceRoll.toString()
+        val resultTV1: TextView = findViewById(R.id.textView1)
+        resultTV1.text = diceR.toString()
     }
 }
 
